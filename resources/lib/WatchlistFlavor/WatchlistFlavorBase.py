46abde09a5ea2ee9f2a7fd3898a7f06145eea434
Hagai Cohen <hagai.co@gmail.com>
2019-01-28 12:53:12 +0200

    Refactor into seperate files
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

---
+++ resources/lib/WatchlistFlavor/WatchlistFlavorBase.py
@@ -0,0 +1,68 @@
+
+class WatchlistFlavorBase(object):
+    _TITLE = None
+    _NAME = None
+    _IMAGE = None
+
+    def __init__(self, login_name, username, password, login_image, login_token):
+        if type(self) is WatchlistFlavorBase:
+            raise Exception("Base Class should not be created")
+
+        self._login_name = login_name
+        self._username = username
+        self._password = password
+        self._login_image = login_image
+        self._login_token = login_token
+
+    @classmethod
+    def name(cls):
+        if cls._NAME is None:
+            raise Exception("Missing Name")
+
+        return cls._NAME
+
+    @property
+    def image(self):
+        if self._IMAGE is None:
+            raise Exception("Missing Image")
+
+        return self._IMAGE
+
+    @property
+    def title(self):
+        if self._TITLE is None:
+            raise Exception("Missing Title")
+
+        return self._TITLE
+
+    @property
+    def login_name(self):
+        return self._login_name
+
+    def login(self):
+        raise NotImplementedError("login should be implemented by subclass")
+
+    def watchlist(self):
+        raise NotImplementedError("watchlist should be implemented by subclass")
+
+    def get_watchlist_status(self, status):
+        raise NotImplementedError("get_watchlist_status should be implemented by subclass")
+
+    def _format_login_data(self, name, image, token):
+        login_data = {
+            "name": name,
+            "image": image,
+            "token": token,
+            }
+
+        return login_data
+
+    def _parse_view(self, base):
+        return [
+            utils.allocate_item("%s" % base["name"],
+                                base["url"],
+                                True,
+                                base["image"],
+                                base["plot"])
+            ]
+
---
+++ resources/lib/WatchlistFlavor/WonderfulSubs.py
@@ -0,0 +1,71 @@
+import itertools
+import json
+import requests
+from ..ui import utils
+from WatchlistFlavorBase import WatchlistFlavorBase
+
+class WonderfulSubsWLF(WatchlistFlavorBase):
+    _TITLE = "WonderfulSubs"
+    _NAME = "wonderfulsubs"
+
+    @property
+    def image(self):
+        return self._login_image
+
+    def login(self):
+        r = requests.post('https://www.wonderfulsubs.com/api/users/login',
+                          json={"username": self._username, "password": self._password})
+
+        data = json.loads(r.text)
+
+        if data['success'] is not True:
+            return
+
+        return self._format_login_data((data['data']['username']),
+                                       (data['data']['profile_pic']),
+                                       ('%s/%s' % (data['data']['_id'], data['token'])))
+
+    def watchlist(self):
+        url = 'https://www.wonderfulsubs.com/api/watchlist/list?_id=%s' % (self._login_token.split("/")[0])
+        return self._process_watchlist_view(url, "watchlist/%d", page=1)
+
+    def header(self):
+        header = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer {}".format(self._login_token.split("/")[1])
+            }
+
+        return header
+
+    def _base_watchlist_view(self, res):
+        result = []
+
+        base = {
+            "name": '%s' % (res['title']),
+            "url": "animes/" + res["url"].replace("/watch/", ""),
+            "image": res['poster'],
+            "plot": '',
+        }
+
+        if res["is_dubbed"]:
+            result.append(utils.allocate_item("%s (Dub)" % base["name"],
+                                              "%s/dub" % base["url"],
+                                              True,
+                                              base["image"],
+                                              base["plot"]))
+        if res["is_subbed"]:
+            result.append(utils.allocate_item("%s (Sub)" % base["name"],
+                                              "%s/sub" % base["url"],
+                                              True,
+                                              base["image"],
+                                              base["plot"]))
+
+        return result
+
+    def _process_watchlist_view(self, url, base_plugin_url, page):
+        r = requests.get(url, headers=self.header())
+        results = json.loads(r.text)['data']['watch_list']
+        all_results = map(self._base_watchlist_view, results)
+        all_results = list(itertools.chain(*all_results))
+        return all_results
