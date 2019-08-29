c6f6ac470de2ce5ad4a07fbddfdb139cd0925176
doko-desuka <31810999+doko-desuka@users.noreply.github.com>
2019-08-29 11:01:48 -0300

    Final commit, I promise (forgot the `xbmc` import)...

--- default.py
+++ default.py
@@ -243,7 +243,7 @@ def SEARCH_ALT(payload, params):
 @route('list_sources/*')
 def LIST_SOURCES(payload, params):
     control.set_property('list_sources', '1')
-    xbmc.executebuiltin('PlayMedia('+payload+')')
+    control.xbmc.executebuiltin('PlayMedia('+payload+')')
 
 @route('play/*')
 def PLAY(payload, params):
