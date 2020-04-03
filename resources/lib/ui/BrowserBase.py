97f108311abeeb329b16baeca97889e456ce8de6
Hagai Cohen <hagai.co@gmail.com>
2020-04-03 14:45:26 +0300

    add support for json post request

--- resources/lib/ui/BrowserBase.py
+++ resources/lib/ui/BrowserBase.py
@@ -11,13 +11,13 @@ class BrowserBase(object):
             url = url[1:]
         return "%s/%s" % (self._BASE_URL, url)
 
-    def _send_request(self, url, data=None, set_request=None):
-        return http.send_request(url, data, set_request).text
+    def _send_request(self, url, data=None, json=None, set_request=None):
+        return http.send_request(url, data, json, set_request).text
 
-    def _post_request(self, url, data={}, set_request=None):
-        return self._send_request(url, data, set_request)
+    def _post_request(self, url, data={}, json=None, set_request=None):
+        return self._send_request(url, data, json, set_request)
 
     def _get_request(self, url, data=None, set_request=None):
         if data:
             url = "%s?%s" % (url, urllib.urlencode(data))
-        return self._send_request(url, None, set_request)
+        return self._send_request(url, None, None, set_request)
